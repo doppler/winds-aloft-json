932fd163fc572daf06d72e4f1ae492b589d9db56
David Rose <doppler@gmail.com>
2018-10-24 22:36:52 -0400

    needs cleanup but good stopping place

--- index.js
+++ index.js
@@ -29,31 +29,75 @@ const parseRows = text => {
 const parseLineToCityAndRawData = line => {
   const d = line.split(/\s/);
   [city, ...rest] = [...d];
-  let cityAndRawForecast = { city, rawForecast: rest };
+  let cityAndRawForecast = {
+    city,
+    rawForecast: rest.slice(0, 4) /* only interested in first 4 altitudes */
+  };
   return cityAndRawForecast;
 };
 
-const humanizeForecast = cityAndRawForecast => {
-  cityAndRawForecast.rawForecast.map((forecast, index) => {
-    const out = {
-      direction: Number(forecast.slice(0, 2)) * 10,
-      speed: Number(forecast.slice(2, 4)),
-      temperature: Number(forecast.slice(4, 7))
-    };
-    if (index > 5) {
-      out.temperature = -out.temperature;
-    }
-    console.log(forecast);
-    console.log(out);
-  });
-};
+// const humanizeForecast = cityAndRawForecast => {
+//   cityAndRawForecast.rawForecast.map((forecast, index) => {
+//     const out = {
+//       direction: Number(forecast.slice(0, 2)) * 10,
+//       speed: Number(forecast.slice(2, 4)),
+//       temperature: Number(forecast.slice(4, 7))
+//     };
+//     if (index > 5) {
+//       out.temperature = -out.temperature;
+//     }
+//     console.log(forecast);
+//     console.log(out);
+//   });
+// };
+
 const parse = html => {
   let parsedRows = parseRows(extractText(html));
-  const cities = [];
-  parsedRows.forecasts.map(line => {
-    let cityAndRawForecast = parseLineToCityAndRawData(line);
-    humanizeForecast(cityAndRawForecast);
-    cities.push(parseLineToCityAndRawData(line));
+  const cities = parsedRows.forecasts.map(line => {
+    return parseLineToCityAndRawData(line);
   });
-  // console.log(cities);
+  humanizeForecast(cities);
+};
+
+const humanizeForecast = cities => {
+  const citiesWithHumanizedForecasts = cities.map(city => {
+    city.furecast = parseForecasts(city.rawForecast);
+    return city;
+  });
+  const atl = citiesWithHumanizedForecasts.find(city => city.city === "ATL");
+  console.log(JSON.stringify(atl, null, 2));
+};
+
+const parseForecasts = forecasts => {
+  return forecasts.map((forecast, i) => {
+    let altitude;
+    switch (i) {
+      case 0:
+        altitude = 3000;
+        break;
+      case 1:
+        altitude = 6000;
+        break;
+      case 2:
+        altitude = 9000;
+        break;
+      case 3:
+        altitude = 12000;
+        break;
+    }
+    let speed = {
+      knots: Number(forecast.slice(2, 4))
+    };
+    speed.mph = Math.round(speed.knots * 1.151);
+    let temperature = {
+      celsius: Number(forecast.slice(4, 7))
+    };
+    temperature.farenheit = Math.round((temperature.celsius * 9) / 5 + 32);
+    return {
+      altitude: altitude,
+      direction: Number(forecast.slice(0, 2)) * 10,
+      speed,
+      temperature
+    };
+  });
 };
