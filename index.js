5d6f8f8d7a80be21cf838600773c5932153a6989
David Rose <doppler@gmail.com>
2018-10-26 12:57:21 -0400

    Add links to individual stations on home page

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const jsonMarkup = require("json-markup");
 const pug = require("pug");
-const regions = require("./regions.json");
+const regionsWithStations = require("./regions-with-stations.json");
 const regionsPage = pug.compileFile("./regionsPage.pug");
 const jsonPage = pug.compileFile("./jsonPage.pug");
-const windsAloftJSON = require("./winds-aloft-json");
+const getWindsAloft = require("./get-winds-aloft");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -12,12 +12,11 @@ const PORT = process.env.PORT || 5000;
 app.use(express.static("assets"));
 
 app.get("/", (req, res) => {
-  res.send(regionsPage({ regions }));
+  res.send(regionsPage({ regionsWithStations }));
 });
 
 app.get("/:region.:ext?/:station?.:ext?", (req, res) => {
-  console.log(req.params);
-  windsAloftJSON(req).then(data => {
+  getWindsAloft(req).then(data => {
     req.params.ext === "json"
       ? res.json(data)
       : res.send(jsonPage({ jsonHTML: jsonMarkup(data), data: data }));
