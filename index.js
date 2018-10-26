ce412e0389368d0c26879befa4eccc7bfa2679fa
David Rose <doppler@gmail.com>
2018-10-26 11:00:12 -0400

    woo pretty

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const jsonMarkup = require("json-markup");
 const pug = require("pug");
 const regions = require("./regions.json");
-const regionsFunction = pug.compileFile("./regions.pug");
+const regionsPage = pug.compileFile("./regionsPage.pug");
+const jsonPage = pug.compileFile("./jsonPage.pug");
 const windsAloftJSON = require("./winds-aloft-json");
 
 const app = express();
@@ -10,12 +12,14 @@ const PORT = process.env.PORT || 5000;
 app.use(express.static("assets"));
 
 app.get("/", (req, res) => {
-  res.send(regionsFunction({ regions }));
+  res.send(regionsPage({ regions }));
 });
 
 app.get("/:region/:station?", (req, res) => {
   windsAloftJSON(req).then(data => {
-    res.send(JSON.stringify(data, undefined, 2));
+    // res.send(JSON.stringify(data, undefined, 2));
+    const jsonHTML = jsonMarkup(data);
+    res.send(jsonPage({ jsonHTML }));
   });
 });
 
